c5232e5b790174bc4f61a14627275999c41671fe
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2023-09-01 10:43:12 -0400

    bootloadCheckCRCCb_

--- src/RVLFaceLib/RFL_Database.c
+++ src/RVLFaceLib/RFL_Database.c
@@ -2,10 +2,30 @@
 
 void RFLiInitDatabase(MEMiHeapHead* heap) {
     RFLiDBManager* mgr = RFLiGetDBManager();
+
     mgr->database =
         (RFLiDatabase*)MEMAllocFromExpHeapEx(heap, sizeof(RFLiDatabase), 32);
     mgr->saveCb = NULL;
     mgr->formatCb = NULL;
     mgr->bootloadCb = NULL;
+
     RFLiClearDBBuffer();
+}
+
+static void bootloadCheckCRCCb_(u32 crc) {
+    RFLiDBManager* mgr = RFLiGetDBManager();
+
+    if (crc != 0) {
+        RFLiGetManager()->lastErrCode = RFLErrcode_Loadfail;
+        RFLiSetFileBroken(RFLiFileBrokenType_DBBroken);
+    } else {
+        RFLiGetManager()->lastErrCode = RFLErrcode_Success;
+    }
+
+    RFLiCloseAsync(RFLiFileType_Database, mgr->bootloadCb);
+}
+
+// https://decomp.me/scratch/PHq7v
+static void bootloadDBcallback_(void) {
+    RFLiDBManager* mgr = RFLiGetDBManager();
 }
